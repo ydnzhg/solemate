b565c55e4d68cfe6c21532355f1dc919df66d34c
ydnzhg <ydnzhg@gmail.com>
2024-01-23 12:32:12 -0500

    added google client id and changed site name

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "FILL ME IN";
+const GOOGLE_CLIENT_ID = "964429567837-ne9qm2vcn91l84kaq31slnqgfrishaaj.apps.googleusercontent.com";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
