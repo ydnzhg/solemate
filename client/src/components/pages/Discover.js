809d4efdea106f6a739b4b229d3b06882894e9f1
ydnzhg <ydnzhg@gmail.com>
2024-01-25 13:44:29 -0500

    added navigation to new discover page

---
+++ client/src/components/pages/Discover.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../utilities.css";
+import "./Discover.css";
+
+const Discover = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage!</h1>
+      {/* Add your homepage content here */}
+    </div>
+  );
+};
+
+export default Discover;
