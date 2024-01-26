e02cbc72da2d0cc6a59eb524595a3eda5f504304
ydnzhg <ydnzhg@gmail.com>
2024-01-26 02:17:28 -0500

    worked on collections page

--- client/src/components/modules/BoardDisplay.js
+++ client/src/components/modules/BoardDisplay.js
@@ -2,26 +2,38 @@ import React from "react";
 
 import "./BoardDisplay.css";
 
+import ShoeCardGrid from "./ShoeCardGrid";
+
 const BoardDisplay = (props) => {
   return (
     <div className="BoardDisplay-container">
       <div className="BoardDisplay-boardInfo">
-        <img src={props.placeholderBoards[props.selectedBoard].image} alt="Board" />
+        <img
+          src={props.placeholderBoards[props.selectedBoard].image}
+          alt="Board"
+          className="BoardDisplay-boardImage"
+        />
         <div className="BoardDisplay-boardText">
-          <h1>{props.placeholderBoards[props.selectedBoard].name}</h1>
-          <p>{props.placeholderBoards[props.selectedBoard].description}</p>
-          <p>Author: {props.placeholderBoards[props.selectedBoard].creator}</p>
+          <p className="BoardDisplay-boardName">
+            {props.placeholderBoards[props.selectedBoard].name}
+          </p>
+          <p className="BoardDisplay-boardDescription">
+            {props.placeholderBoards[props.selectedBoard].description}
+          </p>
+          <p className="BoardDisplay-boardOwner">
+            Author: {props.placeholderBoards[props.selectedBoard].creator}
+          </p>
+        </div>
+        <div className="BoardDisplay-buttonList">
+          <button className="BoardDisplay-button">Edit</button>
+          <button className="BoardDisplay-button">Share</button>
+          <button className="BoardDisplay-button">Export</button>
         </div>
       </div>
-      <div className="BoardDisplay-cardGrid">
-        {props.placeholderBoards[props.selectedBoard].shoes.map((shoe) => (
-          <div key={shoe} className="SmallShoeCard-container">
-            <img src={shoe.image} alt="shoe" className="SmallShoeCard-image" />
-            <p className="SmallShoeCard-brand">{shoe.brand}</p>
-            <p className="SmallShoeCard-name">{shoe.name}</p>
-          </div>
-        ))}
-      </div>
+      <ShoeCardGrid
+        placeholderBoards={props.placeholderBoards}
+        selectedBoard={props.selectedBoard}
+      />
     </div>
   );
 };
