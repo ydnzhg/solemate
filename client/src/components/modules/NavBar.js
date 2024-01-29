00ba6820512fe9f9b32a97980029da12315cd374
ydnzhg <ydnzhg@gmail.com>
2024-01-29 00:13:57 -0500

    moved profile to right side of navbar

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -7,16 +7,18 @@ import "./NavBar.css";
 const NavBar = (props) => {
   return (
     <nav className="NavBar-container">
-      <img src="./logo.png" alt="logo" className="NavBar-logo" />
-      <Link to="/discover" className="NavBar-link">
-        Discover
-      </Link>
-      <Link to="/collections" className="NavBar-link">
-        Collections
-      </Link>
-      <Link to="/explore" className="NavBar-link">
-        Explore
-      </Link>
+      <div className="NavBar-links">
+        <img src="./logo.png" alt="logo" className="NavBar-logo" />
+        <Link to="/discover" className="NavBar-link">
+          Discover
+        </Link>
+        <Link to="/collections" className="NavBar-link">
+          Collections
+        </Link>
+        <Link to="/explore" className="NavBar-link">
+          Explore
+        </Link>
+      </div>
       <div className="NavBar-profile">
         <p>{props.username}</p>
         <img src={props.profilePicture} alt="profile picture" />
